e9c02d17ccfa4ac20341bb76f3de8ebc2918bfab
Jose-Manuel-Zelaya <75284084+Jose-Manuel-Zelaya@users.noreply.github.com>
2021-03-16 22:01:45 -0500

    Add files via upload

---
+++ Terracotta/js/my-app.js
@@ -0,0 +1,38 @@
+// Initialize your app
+var myApp = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: false
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'My App',
+	dialog: {
+		title: 'My App',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/notifications/',
+    	url: 'notifications.html',
+    	name: 'notifications',
+  		},
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
